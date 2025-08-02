d689a62970a45b354eef2407b9b2e703aea45b46
Altamish <altamishpasha@gmail.com>
2025-08-02 11:34:28 +0530

    learned Objected oriented programing

--- opps.cpp
+++ opps.cpp
@@ -1,10 +1,86 @@
 #include <iostream>
+#include <string>
+#include <cstring>
 using namespace std;
 
+class Test {
+public:
+    char* name;
 
-int main(){
+    Test(const char* n){
+        name = new char[strlen(n) + 1];
+        strcpy(name, n);
+    }
+
+    Test(const Test& t){
+        name = new char[strlen(t.name) + 1];
+        strcpy(name, t.name);
+    }
+
+    // Display function
+    void display() {
+        cout << "Name: " << this->name << endl;
+    }
+
+    ~Test(){
+        delete[] name;
+    }
+};
+
+
+class Student {
+    string name;
+    int rollNumber;
+    string classTeacher;
+
+    public: 
+        Student(string n,int r,string c){
+            this->name = n;
+            this->rollNumber = r;
+            this->classTeacher = c;
+        }
+
+    public:
+
+        void setStudent(string n,int r,string c){
+                this->name = n;
+                this->rollNumber = r;
+                this->classTeacher = c;
+        }
+
+        void print(){
+            cout <<"Name: "<< this->name <<endl;
+            cout <<"Roll: "<< this->rollNumber <<endl;
+            cout <<"Class Teacher: "<< this->classTeacher <<endl;
+        }
+
+        string getName(){
+            return this->name;
+        }
+};
+
+
+int main() {
+
+
+    Student s1("altamish",10,"rizwan sir");
+
+    // s1.setStudent("altamish",10,"rizwan sir");
+    // s1.print();
+    cout <<s1.getName();
 
 
 
+    // char name[10] = "Altamish";
+    // Test obj(name);
+
+    // Test obj1(obj);
+
+    // char name1[10] = "rihan bha";
+    // obj1.name = name1;
+
+    // obj.display();
+    // obj1.display();
+    
     return 0;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,8 +1,3 @@
-int arr[5] = {3,5,56,7,4};
-
-    quickSort(arr,0,4);
-
-    for (int i = 0; i < 5; i++)
-    {
-        cout <<arr[i] << " ";
+Test(){
+        name = new char(100);
     }
